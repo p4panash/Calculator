749b65a2efed695594ff2b632759a4cfbee4b1e3
p4panash <muntean.catalin.avram@gmail.com>
2017-08-04 17:48:48 +0300

    //status bar

--- app/src/main/java/manastur/calculator/MainActivity.java
+++ app/src/main/java/manastur/calculator/MainActivity.java
@@ -1,13 +1,29 @@
 package manastur.calculator;
 
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import static manastur.calculator.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(activity_main);
+
+        //changing statusbar color
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimaryDark));
+        }
     }
 }
